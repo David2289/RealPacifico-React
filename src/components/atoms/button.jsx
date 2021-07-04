2e4c62fc79185d3b9aca0797dff6233a92c6203d
David PL <david.2289@icloud.com>
2021-07-04 15:19:10 +0200

    Button component onclick logic - icon option

--- src/components/atoms/button.jsx
+++ src/components/atoms/button.jsx
@@ -1,38 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Button as BootButton } from 'react-materialize';
-import { COLOR } from '../../utils/constants.js'
+import { Button as MatButton } from 'react-materialize';
+import { COLOR } from '../../utils/constants.js';
+import SVG from 'react-inlinesvg';
 
-const ButtonStyled = styled(BootButton)`
+
+const ButtonStyled = styled(MatButton)`
     color: ${COLOR.black};
     background-color: ${COLOR.primary};
     border: none;
     text-transform: none;
     margin: ${props => props.margin ? props.margin : '0 0'};
     &:hover {
-        color: ${COLOR.black};
+        color: ${COLOR.background};
         background-color: ${COLOR.primaryDark};
     }
     &:focus {
-        color: ${COLOR.black};
+        color: ${COLOR.background};
         background-color: ${COLOR.primaryDark};
     }
 `;
 
+const Icon = styled(SVG)`
+    fill: ${COLOR.black};
+    margin: 0 12px;
+    height: 12px;
+`;
+
+
 const Button = (props) => {
     return (
         <ButtonStyled
-            type={props.type}
-            margin={props.margin}>
-            {props.children}
+            onClick={ props.onTapped }
+            margin={ props.margin }>
+            <Icon src={ props.ic_path }/>
+            { props.children }
         </ButtonStyled>
-    );
+    )
 }
 
 Button.propTypes = {
-    type: PropTypes.string, 
-    margin: PropTypes.string
+    onTapped: PropTypes.func,
+    ic_path: PropTypes.string, 
+    margin: PropTypes.string,
 }
 
 export default Button;
